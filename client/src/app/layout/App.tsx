20c7a41def2b32edfa14d932fa4185e80fdf4693
rodwin lising <rodwinlising@gmail.com>
2019-09-27 22:18:39 +1200

    Added react router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,30 +1,37 @@
-import React, { useEffect, Fragment, useContext } from "react";
-import { List, Container } from "semantic-ui-react";
+import React, { Fragment } from "react";
+import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import ActivityStore from '../stores/activityStore';
-import { observer } from 'mobx-react-lite';
+import { observer } from "mobx-react-lite";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App = () => {
-  const activityStore = useContext(ActivityStore);
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) {
-    return <LoadingComponent content="Loading activities..." />
-  }
-
   return (
     <Fragment>
-      <NavBar></NavBar>
-      <List>
-        <Container style={{ marginTop: "7rem" }}>
-          <ActivityDashboard></ActivityDashboard>
-        </Container>
-      </List>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar></NavBar>
+            <Container style={{ marginTop: "7rem" }}>
+              <Route
+                exact
+                path="/activities"
+                component={ActivityDashboard}
+              ></Route>
+              <Route path="/activities/:id" component={ActivityDetails}></Route>
+              <Route
+                path={["/createActivity", "/manage/:id"]}
+                component={ActivityForm}
+              ></Route>
+            </Container>
+          </Fragment>
+        )}
+      ></Route>
     </Fragment>
   );
 };
